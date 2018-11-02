7ef2d3081ef44fefdee6fd26213722c4c0c13a44
ABHISHEK KUMAR SINGH <asociton@outlook.com>
2018-11-02 20:36:09 +0530

    Added get_close_match()
    get_close_matches() suggests words similar to the word entered by user if not found.

--- dict.py
+++ dict.py
@@ -1,8 +1,25 @@
 import json
+import difflib
+from difflib import get_close_matches
 data = json.load(open("data.json"))
-key = input("Enter word ypu want to search for:")
-if data[key]:
-  for i in data[key]:
-    print(i)
-else:
- print("word not present!")
+
+def find_word():
+    key = input("Enter word ypu want to search for:")
+    key  = key.lower()#gets the input in lower case
+    if key in data:
+        for i in data[key]:
+            print(i)
+    elif len(get_close_matches(key,data))>0:
+        print("Did you mean "+get_close_matches(key,data)[0]+" instead!")
+        choi = input("yes/no:")
+        choi = choi.lower()
+        if choi == "yes":
+            key = get_close_matches(key,data)[0]
+            for i in data[key]:
+                print(i)
+        else:
+            print("Sorry we do not have such word !(")
+    else:
+        print("word not present in dictionary :(")
+
+find_word()
