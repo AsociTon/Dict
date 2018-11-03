8a2ce5961ed55c9d87a85a7ad0abfb6cbe198e7f
ABHISHEK KUMAR SINGH <asociton@outlook.com>
2018-11-03 09:44:28 +0530

    Added fix for nouns
    Includes capitalizing the first alphabet to search in data in case of a noun entry also for abbrevations a seperate condition is added.

--- dict.py
+++ dict.py
@@ -6,9 +6,23 @@ data = json.load(open("data.json"))
 def find_word():
     key = input("Enter word ypu want to search for:")
     key  = key.lower()#gets the input in lower case
+    count = 0 #can use key.title() to capitalize the first alphabet
+    key1 = ""
+    for i in key:
+        if count == 0:
+            key1 += i.upper()
+        else:
+            key1 += i
+        count += 1
     if key in data:
         for i in data[key]:
             print(i)
+    elif key1 in data:
+        for i in data[key1]:
+            print(i)
+    elif key.upper() in data:
+         for i in data[key.upper()]:
+             print(i)        
     elif len(get_close_matches(key,data))>0:
         print("Did you mean "+get_close_matches(key,data)[0]+" instead!")
         choi = input("yes/no:")
